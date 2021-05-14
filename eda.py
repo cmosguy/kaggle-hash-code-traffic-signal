8b7156fd91d4903f865da6688f3e88a5ca6cca45
Adam Klein <adam@thinkcru.com>
2021-05-14 14:03:36 -0500

    made a ton of edits and optimized the light routines

--- eda.py
+++ eda.py
@@ -1,107 +1,163 @@
 # %%
+import numpy as np
+import itertools
 import pandas as pd
 from copy import deepcopy
 
 from traffic.traffic import Traffic
+from datetime import datetime
+from tqdm import tqdm
 
 
 # %%
-def make_streets_df(streets):
-    streets_df = pd.DataFrame(
-        columns=['name', 'in_queue', 'start_int', 'time_used'])
+def make_streets_df(streets, intersections_df):
+    streets_dict = {}
+
+    start_time = datetime.now()
+    i = 0
     for street, values in streets.items():
-        streets_df = streets_df.append({'name': street, 'in_queue': len(
-            values.queue), 'start_int': values.start_int, 'time_used': values.time_used}, ignore_index=True)
+        streets_dict[i] = {
+            'name': street,
+            'in_queue': len(values.queue),
+            'start_int': values.start_int,
+            'end_int': values.end_int,
+            'time_used': values.time_used
+            # 'green_light': intersections_df[intersections_df.street_in == street]['green_light'].values[0]
+            # 'single_street_in': intersections_df[intersections_df.street_in == street]['single_street_in'].values[0],
+        }
+        i = i + 1
+
+    streets_df = pd.DataFrame.from_dict(streets_dict, "index")
+    end_time = datetime.now()
+    execution_time = (end_time - start_time).total_seconds() * 1000
+
+    print("Creating streets_df time: {}ms".format(execution_time))
 
     return streets_df
 
 
 def make_intersections_df(intersections):
-    intersections_df = pd.DataFrame(
-        columns=['num', 'street_in', 'green_light_time'])
+    intersections_dict = {}
+
+    start_time = datetime.now()
+    i = 0
     for index, intersection in intersections.items():
         for street_in_index, street_in in enumerate(intersection.streets_in):
-            intersections_df = intersections_df.append(
-                {'num': index, 'street_in': street_in, 'green_light_time': intersection.green_light_time[street_in_index]}, ignore_index=True)
+            intersections_dict[i] = {
+                'intersection_num': index,
+                'street_in': street_in,
+                'green_light_time': intersection.green_light_time[street_in_index],
+                'single_street_in': True if (len(intersection.streets_in) == 1) else False,
+                'green_light': True if (intersection.streets_in[intersection.current_light_index] == street_in) else False
+            }
+            i = i + 1
+
+    intersections_df = pd.DataFrame.from_dict(intersections_dict, "index")
+    end_time = datetime.now()
+    execution_time = (end_time - start_time).total_seconds() * 1000
+
+    print("Creating intersections_df time: {}ms".format(execution_time))
+
     return intersections_df
 
-def make_cars_df(cars, streets_detail_df):
-    cars_df = pd.DataFrame(columns=['name', 'score', 'total_street', 'current_street', 'done'])
+
+def make_cars_df(cars, streets_df):
+    start_time = datetime.now()
+    cars_dict = {}
+
+    i = 0
     for index, car in cars.items():
         current_street_name = car.path[car.current_street]
         done = True if car.current_street == -1 else False
-        cars_df = cars_df.append({'name':car.name, 'score':car.score, 'current_street': current_street_name, 'total_street':car.total_street, 'done':done}, ignore_index=True)
+        cars_dict[i] = {
+            'name': car.name,
+            'score': car.score,
+            'current_street': current_street_name,
+            'total_street': car.total_street,
+            'done': done,
+            'total_travel_time': streets_df[streets_df['name'].isin(car.path)]['time_used'].sum()
+        }
+        i = i + 1
+
+    cars_df = pd.DataFrame.from_dict(cars_dict, "index")
+    end_time = datetime.now()
+    execution_time = (end_time - start_time).total_seconds() * 1000
+
+    print("Creating cars_df time: {}ms".format(execution_time))
+
     return cars_df
 
 
 # %%
-def myCallback(time, cars, streets, streets_details):
+def myCallback(time, cars, streets, streets_details, intersections):
     print('T={}'.format(time))
-    streets_df = make_streets_df(streets=streets)
-    cars_df = make_cars_df(cars=cars, streets_detail_df=streets_details)
+    intersections_df = make_intersections_df(intersections=intersections)
+    streets_df = make_streets_df(streets=streets, intersections_df=intersections_df).join(
+        intersections_df[['street_in', 'single_street_in', 'green_light']].set_index('street_in'), on='name')
+    cars_df = make_cars_df(cars=cars, streets_df=streets_df)
     print(cars_df.head())
-    # print(streets_df.head())
+    print(streets_df.head())
     # cars_df.head()
+
 
 callback = myCallback
 
-#we have to restart the simulation with a clean new class to reinitialize
+# %% we have to restart the simulation with a clean new class to reinitialize
 t = Traffic(in_file='./example.in', truncate_cars=False)
-vector = [2, 1, 2, 1, 1]  #score 1002
-# vector = [1, 1, 1, 1, 1]  
-t.generate_intersection(vector=vector)
-cars = t.simulate(progress_bar=False, override_end_time=None, _callback=callback)
+
+
+t_sl = t.generate_intersection_schedules()
+t.generate_submission_file(intersection_schedule_list=t_sl)
+
+t.read_submission_file()
+
+
+#%%
+
+#vector = [2, 1, 2, 1, 1]  # score 1002
+# vector = [1, 1, 1, 1, 1]
+#t.generate_intersection(vector=vector)
+cars = t.simulate(progress_bar=False,
+                  override_end_time=None, _callback=callback)
 scheduler_score = t.calculate_simulation_score(cars)
 print("Final score: {}".format(scheduler_score))
 
 
+# %% debugging with haschcode.in
+m = Traffic(in_file='./hashcode.in', truncate_cars=2)
+# vector = list(np.random.randint(low=1, high=3, size=len(m.street_detail)))
+# m.generate_intersection(vector=vector)
 # %%
-#debugging with haschcode.in
-import pandas as pd
-from copy import deepcopy
+m_sl = m.generate_intersection_schedules()
+m.generate_submission_file(intersection_schedule_list=m_sl,out_file_path='submission.hashcode.txt')
+m.read_submission_file(in_file_path='submission.hashcode.txt')
 
-from traffic.traffic import Traffic
-m = Traffic(in_file='./hashcode.in')
-# streets_df = make_streets_df(m.streets[:100])
-cars_df = make_cars_df(m.cars, m.street_detail)
-
-#%%
-type(m.streets)
-
-#%%
-
-import itertools
-a = dict(itertools.islice(m.cars.items(),10))
-
-#%%
-cars_df = make_cars_df(m.cars, m.street_detail)
-cars_df.head()
-
-#%%
-m.streets['fifd-bebc'].start_int
-
-#%%
-m.street_detail.describe()
-# %%
-vector = [3, 1, 3, 3, 2]
-t.generate_intersection(vector=vector)
-
-intersections_df = make_intersections_df(t.intersections)
-
-
-intersections_df.head()
-
-#%%
-t.street_detail.head()
-# %%
-import numpy as np
-vector = list(np.random.randint(low=1,high=3, size=len(m.street_detail)))
-m.generate_intersection(vector=vector)
-# %%
 m_cars = m.simulate(progress_bar=True)
-#%%
+# %%
 m_score = m.calculate_simulation_score(m_cars)
 print("Final score: {}".format(m_score))
+
+
+# %%
+int = []
+for c, car in m.cars.items():
+    for street_in_path in car.path:
+        int.append({
+            'street': street_in_path,
+            'timer': 1
+        })
+
+print(int)
+
+
+
+# %%
+
+
+#%%
+        
+
+
 
 
 # %%
--- traffic/traffic.py
+++ traffic/traffic.py
@@ -1,213 +1,489 @@
 from traffic.street import Street
+
 from traffic.intersection import Intersection
+
 from traffic.car import Car
+
 from copy import deepcopy
 
+
+
 import csv
+
 import pandas as pd
+
 from tqdm import tqdm
+
 from tqdm import trange
+
 import itertools
+
+
+
 
 
 class Traffic():
 
+
+
     simulation_config = []
+
     streets = {}
+
     street_detail = []
+
     cars = {}
+
     intersections = []
+
+
 
     def __init__(self, in_file='./hashcode.in', truncate_cars=False):
 
+
+
         self.streets = {}
+
         streets_tmp = {}
+
         self.street_detail = []
+
         self.intersections = []
+
         self.cars = {}
+
         streets_in_path = set()
+
         self.total_intersections = None
+
         self.end_time = 0
 
 
+
+
+
         # Open file ready to read
+
         #f = open(ROOT_DIR + '/hashcode.in', "r")
+
         f = open(in_file, "r")
 
+
+
         # Read simulation configuration
+
         simulation_key = ['D', 'I', 'S', 'V', 'F']
+
         simulation_val = map(int, f.readline().split())
+
         self.simulation_config = dict(zip(simulation_key, simulation_val))
+
         self.total_intersections = self.simulation_config['I']
+
         self.end_time = self.simulation_config['D']+1
 
+
+
         # Read streets detail
+
         for _ in range(self.simulation_config['S']):
+
             type_converted_line = list(
+
                 map(self.convert_type, f.readline().split()))
+
             self.street_detail.append(type_converted_line)
+
             new_street = Street(
+
                 type_converted_line[0], type_converted_line[1], type_converted_line[2], type_converted_line[3])
+
             streets_tmp[type_converted_line[2]] = new_street
+
+
 
         self.street_detail = pd.DataFrame(self.street_detail, columns=['start_int', 'end_int', 'name', 'time_used'])
 
+
+
         print("Successfully read {} streets detail".format(self.simulation_config['S']))
 
+
+
         # Read cars path
+
         for i in range(self.simulation_config['V']):
+
             type_converted_line = list(
+
                 map(self.convert_type, f.readline().split()))
+
             type_converted_line = [
+
                 type_converted_line[0], type_converted_line[1:]]
+
             streets_in_path = streets_in_path.union(type_converted_line[1])
+
             car_name = 'car' + str(i)
+
             new_car = Car( car_name, type_converted_line[0], type_converted_line[1])
+
             self.cars[car_name] = new_car
+
+
 
         print("Successfully read {} cars paths".format(len(self.cars)))
 
+
+
         # Remove street with no cars
+
         orig_street_cnt = len(streets_tmp)
+
         self.street_detail = self.street_detail[self.street_detail['name'].isin(streets_in_path)]
+
         self.streets = {street: streets_tmp[street] for street in streets_in_path}
+
         self.simulation_config['S'] = len(self.streets)
 
+
+        self.generate_intersection()
+
+
         print("Successfully removed {} streets with no cars".format(orig_street_cnt - len(self.streets)))
+
         print("Total streets: {}".format(len(self.streets)))
+
         print("Total cars: {}".format(len(self.cars)))
+
         print("Total intersections: {}".format(self.total_intersections))
+
         print("Original endtime: {}".format(self.end_time-1))
 
+
+
         if truncate_cars:
+
             self.cars = dict(itertools.islice(self.cars.items(),truncate_cars))
+
             print('Truncated cars to total of: {}'.format(len(self.cars.items())))
 
 
-    # Generate intersections
-    def generate_intersection(self, scheduler=None, vector=None):
 
-        if (scheduler is None) and (vector):
-            scheduler = pd.DataFrame({'street_name': deepcopy(self.street_detail['name']), 'green_time': vector})
-        elif (scheduler is None) and (vector is None):
-            exit(2)
+
+
+    # Generate intersections
+
+    def generate_intersection(self):
+
+
 
         intersects = {}
-        for i in range(self.simulation_config['I']):
-            streets_in = deepcopy(
-                list(self.street_detail.loc[self.street_detail['end_int'] == i, 'name']))
-            #   Sort the list respect to scheduler order
-            streets_in = deepcopy(
-                list(scheduler[scheduler['street_name'].isin(streets_in)]['street_name']))
+
+        for i in range(self.total_intersections):
+
+            streets_in = deepcopy(list(self.street_detail.loc[self.street_detail['end_int'] == i, 'name']))
+
             if len(streets_in) > 0:
-                green_time = deepcopy(
-                    list(scheduler[scheduler['street_name'].isin(streets_in)]['green_time']))
+
+                green_time = []
+
                 intersects[i] = Intersection(i, streets_in, green_time)
 
         self.intersections = deepcopy(intersects)
-        #print("Loaded {} intersections".format(len(self.intersections)), end='\n')
+
+        print("Loaded {} intersections".format(len(self.intersections)), end='\n')
+
+
 
     # Used to calculate simulation score
+
     def calculate_simulation_score(self, cars):
+
         total_score = 0
+
         for car in cars.values():
+
             total_score = total_score + car.score
+
         return total_score
 
+
+
     # # Change the scheduler of intersections
+
     # def change_scheduler(intersections, scheduler):
+
     #     for intersection in intersections.values():
+
     #         intersection.plug_scheduler(scheduler)
+
     #     return deepcopy(intersections)
 
+
+
     # Function for type convertion
+
     def convert_type(self, attr):
+
         try:
+
             return int(attr)
+
         except:
+
             return attr
+
+
 
     # Return time used in each street
+
     def get_time_used(self, street_detail, street_name):
+
         return street_detail.loc[street_detail['name'] == street_name, 'time_used']
 
+
+
     # # Generate output file
+
     # def generate_output_file(intersections):
+
     #     with open('submission.csv', 'w') as file:
+
     #         file.write("{}\n".format(int(len(intersections))))
+
     #         for intersection in intersections.values():
+
     #             file.write("{}\n".format(int(intersection.name)))
+
     #             file.write("{}\n".format(int(len(intersection.streets_in)) ))
+
     #             for street, green_time in zip(intersection.streets_in, intersection.green_light_time):
+
     #                 file.write("{} {}\n".format(str(street), int(green_time)))
 
+
+
     # Generate output file
+
     def generate_output_file(self, outfile='submission.csv'):
+
         with open(outfile, 'w', newline='') as file:
+
             writer = csv.writer(file)
+
             writer.writerow([int(len(self.intersections))])
+
             for intersection in self.intersections.values():
+
                 writer.writerow([int(intersection.name)])
+
                 writer.writerow([int(len(intersection.streets_in))])
+
                 for street, green_time in zip(intersection.streets_in, intersection.green_light_time):
+
                     writer.writerow(
+
                         ["{} {}".format(str(street), int(green_time))])
 
+
+
     def convert_type(self, attr):
+
         try:
+
             return int(attr)
+
         except:
+
             return attr
+
+
 
     # Function to simulate the traffic flow
 
+
+
     def simulate(self, override_end_time=None, progress_bar = False,_callback=None):
+
         #     print("Starting traffic simulation.")
 
+
+
         #   Intial cars on streets
+
         for car in self.cars.values():
+
             init_street = car.path[0]
+
             self.streets[init_street].add_queue(car.name, True)
+
             car.new_street_flag = False
 
+
+
         #   Simulate cars move
+
         if override_end_time:
+
             self.end_time = override_end_time+1
+
             print("Override end time: {}".format(self.end_time-1))
 
+
+
         tprog = range
+
         if progress_bar:
+
             tprog = trange
 
 
+
+
+
         for T in tprog(self.end_time):
+
             # for each car reset the new street flag
+
             for car in self.cars.values():
+
                 car.new_street_flag = False
+
+
 
             for intersection in self.intersections.values():
 
+
+
                 #   Find streets which currently green and red light
+
                 green_street, red_streets = intersection.update_light()
 
+
+
                 #   Update cars on street with green signal
+
                 car_to_new_street = self.streets[green_street].update_cars_move(self.cars, True)
 
+
+
                 #   Update cars on streets with red signal
+
                 for red_street in red_streets:
+
                     self.streets[red_street].update_cars_move(self.cars, False)
 
+
+
                 #   Update cars path
+
                 if (car_to_new_street is not None):
+
                     # if(self.cars[car_to_new_street].new_street_flag == False):
+
                     new_street = self.cars[car_to_new_street].update_current_street()
+
                     if new_street is not None:
+
                         self.streets[new_street].add_queue(car_to_new_street, False)
+
                     else:
+
                         self.cars[car_to_new_street].update_score(self.simulation_config['F'], self.end_time, T)
+
     #                     print("{} has reached the destination and received {} points.".format(car_to_new_street, car_score))
+
                 
+
                 # pbar.update(round(100/((end_time+1)/(T+1))))
+
             if _callback:
-                _callback(T, deepcopy(self.cars), deepcopy(self.streets), deepcopy(self.street_detail))
+
+                _callback(T, deepcopy(self.cars), deepcopy(self.streets), deepcopy(self.street_detail), deepcopy(self.intersections))
+
         return deepcopy(self.cars)
+
+    def generate_intersection_schedules(self):
+        def byGreenSeconds(e):
+            return e['green_seconds']
+
+        def byNumberIncomingStreets(s):
+            return s['num_incoming_streets']
+
+        intersection_schedule_list = []
+        for index, intersection in tqdm(self.intersections.items()):
+
+            intersection_schedule = {
+                'order_duration_green_lights': [],
+                'id': index,
+                'num_incoming_streets': len(intersection.streets_in)
+            }
+
+            for street in intersection.streets_in:
+
+                is_start_of_path = 0
+                for c, car in self.cars.items():
+                    if car.path[0] == street:
+                        is_start_of_path = 1
+
+                order_duration_green_light = { 
+                    'street_name': street, 
+                    'green_seconds': 1+is_start_of_path
+                    }
+
+                intersection_schedule['order_duration_green_lights'].append(order_duration_green_light)
+
+            if (len(intersection_schedule['order_duration_green_lights'])):
+                intersection_schedule['order_duration_green_lights'].sort(reverse=True, key=byGreenSeconds)
+
+                intersection_schedule_list.append(intersection_schedule)
+                intersection_schedule_list.sort(reverse=True, key=byNumberIncomingStreets)
+
+                # intersection_percentage = len(intersection_schedule_list) / len(self.intersections)
+
+                # print("{} / {} = {}".format(len(intersection_schedule_list), len(self.intersections), intersection_percentage ))
+            
+        return intersection_schedule_list
+
+    def generate_submission_file(self, intersection_schedule_list, out_file_path='submit.example.txt'):
+        with open(out_file_path, 'w') as out_file:
+            out_file.write("{}\n".format(len(intersection_schedule_list)))
+            for intersection_schedule in intersection_schedule_list:
+                out_file.write("{}\n{}\n".format(intersection_schedule['id'], intersection_schedule['num_incoming_streets']))
+                for order_duration_green_light in intersection_schedule['order_duration_green_lights']:
+                    out_file.write("{} {}\n".format(order_duration_green_light['street_name'], order_duration_green_light['green_seconds']))
+
+        print("\nWrote to file: {}".format(out_file_path, end="\n"))
+
+    def read_submission_file(self, in_file_path='submit.example.txt'):
+        in_file = open(in_file_path, 'r')
+        lines = in_file.readlines()
+
+        i = 0 
+        intersection_num = 0
+        green_light_flag = False
+        while i < len(lines):
+            if(i==0):
+                total_intersections = int(lines[0].strip()) * [None]
+                i += 1
+            elif(i>0 and green_light_flag == False):
+                intersection_num = int(lines[i].strip())
+                green_light_flag = True
+                i += 1
+            else:
+                num_streets = int(lines[i].strip())
+                i += 1
+                street_names = num_streets * [None]
+                green_seconds = num_streets * [None]
+                for l in range(num_streets):
+                    street_names[l], green_seconds_str = lines[i+l].strip().split(' ')
+                    green_seconds[l] = int(green_seconds_str)
+                
+                i += num_streets
+
+                green_light_flag = False
+                self.intersections[intersection_num].streets_in = street_names
+                self.intersections[intersection_num].green_light_time = green_seconds
+
+
